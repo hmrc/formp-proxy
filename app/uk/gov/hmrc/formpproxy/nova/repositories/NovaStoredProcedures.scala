b604a17ed80506f313c8167e4a8b0cb8e9e4de41
kara <karahanozturk@gmail.com>
2026-04-14 09:36:48 +0100

    DTR-4129 Qualify stored procedure names with NOVA_DATA schema

--- app/uk/gov/hmrc/formpproxy/nova/repositories/NovaStoredProcedures.scala
+++ app/uk/gov/hmrc/formpproxy/nova/repositories/NovaStoredProcedures.scala
@@ -17,12 +17,13 @@
 package uk.gov.hmrc.formpproxy.nova.repositories
 
 object NovaStoredProcedures {
-  val CallGetForms               = "{ call COMMON_FORMS_PACKAGE.getForms(?, ?, ?, ?) }"
-  val CallGetForm                = "{ call COMMON_FORMS_PACKAGE.getForm(?, ?) }"
-  val CallStoreForm              = "{ call COMMON_FORMS_PACKAGE.storeForm(?, ?, ?, ?, ?) }"
-  val CallUpdateFormStatus       = "{ call COMMON_FORMS_PACKAGE.updateFormStatus(?, ?, ?, ?, ?) }"
-  val CallDeleteForm             = "{ call COMMON_FORMS_PACKAGE.deleteForm(?) }"
-  val CallGetNovaNotificationRef = "{ call NOVA_NOTIF_REF_GENERATOR.getNovaNotificationRef(?, ?, ?, ?, ?, ?) }"
-  val CallGetFormData            = "{ call COMMON_FORMS_PACKAGE.getFormData(?, ?, ?) }"
-  val CallStoreFormData          = "{ call COMMON_FORMS_PACKAGE.storeFormData(?, ?, ?, ?, ?) }"
+  val CallGetForms               = "{ call NOVA_DATA.COMMON_FORMS_PACKAGE.getForms(?, ?, ?, ?) }"
+  val CallGetForm                = "{ call NOVA_DATA.COMMON_FORMS_PACKAGE.getForm(?, ?) }"
+  val CallStoreForm              = "{ call NOVA_DATA.COMMON_FORMS_PACKAGE.storeForm(?, ?, ?, ?, ?) }"
+  val CallUpdateFormStatus       = "{ call NOVA_DATA.COMMON_FORMS_PACKAGE.updateFormStatus(?, ?, ?, ?, ?) }"
+  val CallDeleteForm             = "{ call NOVA_DATA.COMMON_FORMS_PACKAGE.deleteForm(?) }"
+  val CallGetNovaNotificationRef =
+    "{ call NOVA_DATA.NOVA_NOTIF_REF_GENERATOR.getNovaNotificationRef(?, ?, ?, ?, ?, ?) }"
+  val CallGetFormData            = "{ call NOVA_DATA.COMMON_FORMS_PACKAGE.getFormData(?, ?, ?) }"
+  val CallStoreFormData          = "{ call NOVA_DATA.COMMON_FORMS_PACKAGE.storeFormData(?, ?, ?, ?, ?) }"
 }
