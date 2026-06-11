e259d057b09ba3a0d7025564e2277946c42106a7
szaniszlo <15050363+szaniszlo@users.noreply.github.com>
2026-06-11 18:27:51 +0100

    DTR-5565: cover the 204 scenarios in Verification IT

--- it/test/uk/gov/hmrc/formpproxy/cis/VerificationControllerIntegrationSpec.scala
+++ it/test/uk/gov/hmrc/formpproxy/cis/VerificationControllerIntegrationSpec.scala
@@ -16,18 +16,38 @@
 
 package uk.gov.hmrc.formpproxy.cis
 
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.when
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import org.scalatest.matchers.must.Matchers
-import uk.gov.hmrc.formpproxy.itutil.ApplicationWithWiremock
+import play.api.Application
 import play.api.http.Status.*
+import play.api.inject.bind
+import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.libs.json.{JsNull, Json}
-import uk.gov.hmrc.formpproxy.itutil.AuthStub
+import uk.gov.hmrc.formpproxy.cis.repositories.CisMonthlyReturnSource
+import uk.gov.hmrc.formpproxy.itutil.{ApplicationWithWiremock, AuthStub}
+
+import scala.concurrent.Future
 
 class VerificationControllerIntegrationSpec
     extends Matchers
     with ScalaFutures
     with IntegrationPatience
     with ApplicationWithWiremock {
+
+  private lazy val stubRepo: CisMonthlyReturnSource = {
+    val m = org.mockito.Mockito.mock(classOf[CisMonthlyReturnSource])
+    when(m.modifyVerifications(any())).thenReturn(Future.successful(()))
+    when(m.updateVerificationSubmission(any())).thenReturn(Future.successful(()))
+    m
+  }
+
+  override lazy val app: Application = new GuiceApplicationBuilder()
+    .configure(extraConfig)
+    .overrides(bind[CisMonthlyReturnSource].toInstance(stubRepo))
+    .build()
+
   "GET /formp-proxy/cis/verification-batch/current/:instanceId " should {
 
     "return 401 when there is no active session" in {
@@ -78,6 +98,27 @@ class VerificationControllerIntegrationSpec
       )
 
       res.status mustBe UNAUTHORIZED
+    }
+
+    "return 204 when the request is valid" in {
+      AuthStub.authorised()
+
+      val res = postAwait(
+        endpoint,
+        Json.obj(
+          "instanceId"          -> "abc-123",
+          "deleteVerifications" -> Json.obj(
+            "verificationResourceReferences" -> Seq(111L, 222L)
+          ),
+          "createVerifications" -> Json.obj(
+            "verificationBatchResourceRef"   -> 10L,
+            "verificationResourceReferences" -> Seq(333L, 444L),
+            "actionIndicator"                -> JsNull
+          )
+        )
+      )
+
+      res.status mustBe NO_CONTENT
     }
 
     "return 404 for unknown endpoint (routing sanity)" in {
@@ -131,6 +172,25 @@ class VerificationControllerIntegrationSpec
       )
 
       res.status mustBe UNAUTHORIZED
+    }
+
+    "return 204 when the request is valid" in {
+      AuthStub.authorised()
+
+      val res = postAwait(
+        endpoint,
+        Json.obj(
+          "instanceId"                   -> "abc-123",
+          "verificationBatchId"          -> 99L,
+          "verificationBatchResourceRef" -> 77L,
+          "submittableStatus"            -> "FATAL_ERROR",
+          "govtalkErrorCode"             -> "500",
+          "govtalkErrorType"             -> "timeOut",
+          "govtalkErrorMessage"          -> "timeOut"
+        )
+      )
+
+      res.status mustBe NO_CONTENT
     }
 
     "return 404 for unknown endpoint (routing sanity)" in {
