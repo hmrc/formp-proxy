cac04ba5508d0ed40df6497448dd119de7053e24
yohan-oc <129070827+yohan-oc@users.noreply.github.com>
2026-08-13 14:59:48 +0100

    DTR-7415: tidy up

--- test/uk/gov/hmrc/formpproxy/cis/services/VerificationServiceSpec.scala
+++ test/uk/gov/hmrc/formpproxy/cis/services/VerificationServiceSpec.scala
@@ -102,7 +102,7 @@ class VerificationServiceSpec extends SpecBase {
   "VerificationService#getCurrentVerificationBatch" - {
 
     "return successful response from repo" in {
-      val c = Ctx();
+      val c = Ctx()
       import c.*
 
       val instanceId = "abc-123"
@@ -124,7 +124,7 @@ class VerificationServiceSpec extends SpecBase {
     }
 
     "propagates failure from repo" in {
-      val c = Ctx();
+      val c = Ctx()
       import c.*
 
       val instanceId = "abc-123"
@@ -309,7 +309,7 @@ class VerificationServiceSpec extends SpecBase {
   "VerificationService#createSubmissionForVerification" - {
 
     "delegates to repository" in {
-      val c = Ctx();
+      val c = Ctx()
       import c.*
 
       val req = CreateSubmissionAndUpdateVerificationsRequest(
@@ -345,7 +345,7 @@ class VerificationServiceSpec extends SpecBase {
     }
 
     "propagates failure from repository" in {
-      val c = Ctx();
+      val c = Ctx()
       import c.*
 
       val req = CreateSubmissionAndUpdateVerificationsRequest(
@@ -446,7 +446,7 @@ class VerificationServiceSpec extends SpecBase {
   "VerificationService#updateVerificationSubmission" - {
 
     "delegates to repository" in {
-      val c = Ctx();
+      val c = Ctx()
       import c.*
 
       val req = UpdateVerificationSubmissionRequest(
@@ -470,7 +470,7 @@ class VerificationServiceSpec extends SpecBase {
     }
 
     "propagates failure from repository" in {
-      val c = Ctx();
+      val c = Ctx()
       import c.*
 
       val req = UpdateVerificationSubmissionRequest(
@@ -600,7 +600,7 @@ class VerificationServiceSpec extends SpecBase {
   "VerificationService#proceedVerification" - {
 
     "delegates to repository" in {
-      val c = Ctx();
+      val c = Ctx()
       import c.*
 
       val req = ProceedVerificationRequest(
@@ -621,7 +621,7 @@ class VerificationServiceSpec extends SpecBase {
     }
 
     "propagates failure from repository" in {
-      val c = Ctx();
+      val c = Ctx()
       import c.*
 
       val req = ProceedVerificationRequest(
