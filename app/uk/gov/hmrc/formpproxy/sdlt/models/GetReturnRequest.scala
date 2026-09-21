1772d85229f29497be323c761b30b5ba05a5606b
Scott Goodwin <33629936+scott-goodwin@users.noreply.github.com>
2026-09-21 11:19:17 +0100

    Added functionalty to return set of errors instead of the first one for SDLT (#144)

--- app/uk/gov/hmrc/formpproxy/sdlt/models/GetReturnRequest.scala
+++ app/uk/gov/hmrc/formpproxy/sdlt/models/GetReturnRequest.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 HM Revenue & Customs
+ * Copyright 2026 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -403,7 +403,7 @@ case class GetReturnRequest(
   lease: Option[Lease] = None,
   taxCalculation: Option[TaxCalculation] = None,
   submission: Option[Submission] = None,
-  submissionErrorDetails: Option[SubmissionErrorDetails] = None,
+  submissionErrorDetails: Option[Seq[SubmissionErrorDetails]] = None,
   residency: Option[Residency] = None
 )
 
